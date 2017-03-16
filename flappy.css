b97f9577bb74f5cf4250bf78a825f08d216cf884
Mateus Nunes de B Magalhaes <mateusnbm@Mateuss-MBP.home>
2017-03-15 21:08:49 -0300

    Initial Commit

--- flappy.css
+++ flappy.css
@@ -20,7 +20,7 @@
 
 	MIT License
 
-	Copyright (c) 2017 Mateus
+	Copyright (c) 2017 Mateusnbm
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
